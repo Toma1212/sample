6e024099939173e90aee66e7edb6045c0c8ad1cd
Toma1212 <qd30755778@outlook.jp>
2020-11-09 18:46:53 +0900

    apitest

---
+++ laravel/resources/js/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="footer">
+        <b-button variant="danger">Logout</b-button>
+        <RouterLink class="btn btn-danger" to="/login">
+            Login / Register
+        </RouterLink>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
